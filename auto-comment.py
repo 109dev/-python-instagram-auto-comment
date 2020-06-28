b5a7b8cb2fb2e519ace1549551ea47ca93f42714
서원 <dev109@kakao.com>
2020-06-28 12:10:19 +0900

    기본기능구현

---
+++ auto-comment.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from myid import ID, PW
+import time
+
+driver = webdriver.Chrome('./chromedriver')
+
+try:
+    driver.get('https://instagram.com')
+
+    time.sleep(2) #페이지 로딩이 느려지면 다음에 실행 될 동작들이 실행이 될 수 없어 에러가 나기때문에 시간이 지체되는 부분에 대기 시간을 넣어줍니다.
+
+    #login
+    login_id = driver.find_element_by_name('username')
+    login_id.send_keys(ID)
+    login_pw = driver.find_element_by_name('password')
+    login_pw.send_keys(PW)
+    login_pw.send_keys(Keys.RETURN)
+
+    time.sleep(5)
+
+    #pass popup - 로그인 후 피드 페이지에서 팝업창이 따로 나오지 않을경우 아래 부분은 주석하고 바로 해시태그 검색
+    popup = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')
+    popup.send_keys(Keys.ENTER)
+    time.sleep(3)
+    popup = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+    popup.send_keys(Keys.ENTER)
+
+    time.sleep(2)
+
+    #searh
+    search = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+    search.send_keys('#젖소냥이')
+    time.sleep(5)
+    search = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]')
+    feedCtn = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]/div/div/div[2]/span/span').text
+    print('검색된 피드 수 : {}'.format(feedCtn))
+    search.send_keys(Keys.ENTER)
+
+    time.sleep(3)
+
+    #select first feed
+    feed = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a')
+    feed.send_keys(Keys.ENTER)
+
+    time.sleep(5)
+
+    feedCtn = 10 #전체 피드 수 만큼 반복하시려면 feedCtn을 따로 지정하지 않고, 검색부분에서 가져온 feedCtn을 그대로 사용합니다.
+    while True:
+        #comment
+        comm = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div[1]/form/textarea')
+
+        ac = ActionChains(driver)
+        ac.move_to_element(comm)
+        ac.click()
+        ac.pause(3)
+        ac.send_keys('귀여워요!')
+        ac.pause(1)
+        ac.send_keys(Keys.ENTER)
+        ac.perform()
+
+
+        time.sleep(3)
+
+        nextFeed = driver.find_element_by_css_selector('body > div._2dDPU.CkGkG > div.EfHg9 > div > div > a._65Bje.coreSpriteRightPaginationArrow')
+        ac = ActionChains(driver)
+        ac.move_to_element(nextFeed)
+        ac.click()
+        ac.perform()
+
+        if feedCtn == 1:
+            break
+        time.sleep(1)
+except Exception as e:
+    print(e)
+finally:
+    driver.quit()
